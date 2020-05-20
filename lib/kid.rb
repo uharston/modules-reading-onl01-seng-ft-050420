dbf871530454f371a06e3875371770dbef5ddf69
Uriah Harston <uharston@gmail.com>
2020-05-20 18:51:04 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,13 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class Kid 
-  include Dance 
+class Kid
+  include Dance
 
-  attr_accessor :name 
+  attr_accessor :name
 
   def initialize(name)
-    @name =name 
-  end 
+    @name =name
+  end
 
 end 
